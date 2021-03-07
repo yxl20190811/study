3aee21ecfa4d713fc6a88cd4da54eed6c49d7882
yy20190811 <yy20190811@126.com>
2021-03-08 07:34:05 +0800

    遍历文件，得到"目录下的所有class文件名， 作为java类名称，放到"m_classNameList“中

--- springboot/mini-spingboot/src/main/java/com/example/minispingboot/MiniSpingbootApplication.java
+++ springboot/mini-spingboot/src/main/java/com/example/minispingboot/MiniSpingbootApplication.java
@@ -3,10 +3,15 @@ package com.example.minispingboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.servlet.ServletException;
+
 @SpringBootApplication
 public class MiniSpingbootApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ServletException {
+        TMiniSpringboot pThis = new TMiniSpringboot();
+        pThis.init();
+        String packName = pThis.getClass().getPackageName();
         SpringApplication.run(MiniSpingbootApplication.class, args);
     }
 
