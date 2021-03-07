65dd656702dbd7040041f451d6394d437f673a66
yy20190811 <yy20190811@126.com>
2021-03-07 22:52:15 +0800

    写mini-springboot的原始类TMiniSpringboot

---
+++ springboot/mini-spingboot/src/main/java/com/example/minispingboot/MiniSpingbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.minispingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniSpingbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniSpingbootApplication.class, args);
+    }
+
+}
---
+++ springboot/mini-spingboot/src/test/java/com/example/minispingboot/MiniSpingbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minispingboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSpingbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
