23e0842461d6752a92bfe5469638e888268b80de
yy20190811 <yy20190811@126.com>
2021-03-10 22:20:19 +0800

    增加MySpringBoot提换mini-springboot

---
+++ springboot/MySpringBoot/src/main/java/TMain.java
@@ -0,0 +1,24 @@
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+
+public class TMain {
+    static final int port = 9080;
+    static final String docBase = "c:\\";
+    public static void main(String[] args) throws Exception {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(port);
+        tomcat.setBaseDir(docBase);
+        tomcat.getHost().setAutoDeploy(false);
+
+        String contextPath = "/book";
+        StandardContext context = new StandardContext();
+        context.setPath(contextPath);
+        context.addLifecycleListener(new Tomcat.FixContextListener());
+        tomcat.getHost().addChild(context);
+
+        tomcat.addServlet(contextPath, "homeServlet", new TMySpringBoot());
+        context.addServletMappingDecoded("/home", "homeServlet");
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
