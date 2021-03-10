086b100aaf22b13caf57c8e800db3abffa84e764
yy20190811 <yy20190811@126.com>
2021-03-10 22:39:49 +0800

    增加init函数

--- springboot/MySpringBoot/src/main/java/TMySpringBoot.java
+++ springboot/MySpringBoot/src/main/java/TMySpringBoot.java
@@ -1,12 +1,19 @@
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class TMySpringBoot  extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("request scheme: " + req.getScheme());
